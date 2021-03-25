ce2ad9d1a3ec36b6e5e5c7e6e4ad5e4c319ca821
unknown <ibmjavanh07@iiht.tech>
2021-03-25 17:51:16 +0530

    added validation logic in OrderServices,updated methods

--- src/main/java/comibm/demo/OrderController.java
+++ src/main/java/comibm/demo/OrderController.java
@@ -1,16 +1,21 @@
 package comibm.demo;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.validation.BindingResult;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import comibm.demo.entity.Order;
@@ -21,31 +26,42 @@ public class OrderController { //frontend
 	@Autowired    //used for DI
 	OrderService orderService;  //DI
 	@PostMapping("/order")
+	@ResponseStatus(code=HttpStatus.CREATED)
 	String createOrder(@RequestBody @Valid Order order , BindingResult bindingResult) {
-		if(bindingResult.hasErrors()) {
-			throw new IllegalArgumentException("Something went wrong, please retry!");
-		}
+		validateModel(bindingResult);
 		System.out.println(order);
 		return orderService.createOrder(order);
 	}
-
+	
 	@GetMapping("/order")
-	String getOrder() {
-		return "Order created";
+	List<Order> getOrders() {
+		return orderService.getOrders();
+	}
+	
+	@GetMapping("/order/{id}")
+	Order getOrder( @PathVariable("id") int orderId) {
+		 return orderService.getOrder(orderId);
+	}
+	
+	private void validateModel(Errors bindingResult) {
+		if(bindingResult.hasErrors()) {
+			throw new IllegalArgumentException("Something went wrong, please retry!");
+		}
 	}
 
 	@PutMapping("/order/{id}")
-	String updateOrder(@PathVariable("id") int orderId) {
+	void updateOrder(@RequestBody @Valid Order order, @PathVariable("id") int orderId, BindingResult bindingResult ) {
+		validateModel(bindingResult);
 		System.out.println(orderId);
-		return "order updated";
+		orderService.updateOrder(orderId);
 
 	}
 
 	@DeleteMapping("/order/{id}")
-	String deleteOrder(@PathVariable("id") int orderId, HttpRequest httpRequest) {
+	void deleteOrder(@PathVariable("id") int orderId, HttpRequest httpRequest) {
 		System.out.println(httpRequest.getHeaders());
 		System.out.println(orderId);
-		return "order deleted";
+		orderService.deleteOrder(orderId);
 
 	}
 
--- src/main/java/comibm/demo/service/OrderService.java
+++ src/main/java/comibm/demo/service/OrderService.java
@@ -1,5 +1,8 @@
 package comibm.demo.service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import comibm.demo.entity.Order;
@@ -10,5 +13,24 @@ public class OrderService {  //spring bean
 	public String createOrder(Order order) {
 		return "order created";
 	}
+	
+	public List<Order>getOrders(){
+		return new ArrayList<Order>();
+	}
+	
+	public Order getOrder(int id) {
+		return new Order();
+	}
+
+	public void updateOrder(int id) {
+		// TODO Auto-generated method stub
+		//return "order updated";
+	}
+
+	public void deleteOrder(int orderId) {
+		// TODO Auto-generated method stub
+		System.out.println("Order deleted");
+	}
+	
 
 }
