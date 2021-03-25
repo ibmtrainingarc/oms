5210248c433fe838d66a89e3761dc4dc88fa64d4
unknown <ibmjavanh07@iiht.tech>
2021-03-25 09:22:00 +0530

    first commit

---
+++ src/test/java/comibm/demo/SpringbootDemo1ApplicationTests.java
@@ -0,0 +1,13 @@
+package comibm.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDemo1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
